307d0235fca414536afd42e088ece2968f7d67e9
ekintaskin <ekin.taskin@epfl.ch>
2026-02-15 16:34:44 +0100

    HARDI SSST test

--- tests/test.py
+++ tests/test.py
@@ -7,65 +7,70 @@ import numpy as np
 from dipy.core.gradients import gradient_table
 from dipy.data import get_fnames
 from dipy.io.gradients import read_bvals_bvecs
-from dipy.io.image import load_nifti
-from dipy.reconst.csdeconv import ConstrainedSphericalDeconvModel, auto_response_ssst
-from dipy.segment.mask import median_otsu
+from dipy.io.image import load_nifti, save_nifti
+from dipy.reconst.csdeconv import response_from_mask_ssst
 import nibabel as nib
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+from dipy.denoise.localpca import mppca
+from pathlib import Path
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT / "src"))
 
 from sr2csd.utils.sh_basis import convert_sh_descoteaux_tournier
-from sr2csd import SR2CSDModel
+from sr2csd.utils.response import select_voxel
+from sr2csd import MRtrixCSDModel, SR2CSDModel
 
 def main() -> None:
-    data, data_affine = load_nifti(r"/home/ekin/Desktop/paper_analysis3/hardi/snr15/set10/denoised_hardi_snr15_set10.nii.gz")
-    bvals = np.loadtxt(r"/home/ekin/Documents/data/hardi/hardi-scheme.bval", dtype=float)
-    bvecs = np.loadtxt(r"/home/ekin/Documents/data/hardi/hardi-scheme.bvec", dtype=float)
+    data, data_affine = load_nifti(f"{ROOT}/data/hardi/denoised_hardi_SNR-20.nii.gz")
+    bvals = np.loadtxt(f"{ROOT}/data/hardi/hardi-scheme.bval", dtype=float)
+    bvecs = np.loadtxt(f"{ROOT}/data/hardi/hardi-scheme.bvec", dtype=float)
     gtab = gradient_table(bvals, bvecs)
 
-    mask, _ = load_nifti(r"/home/ekin/Documents/data/hardi/mask_compute_local_metrics.nii.gz")
+    mask, _ = load_nifti(f"{ROOT}/data/hardi/mask.nii")
 
-    b0_idx = np.flatnonzero(bvals < 50)
+    # denoise data
+    #data = mppca(data_noisy, patch_radius=2)
+    #save_nifti(f"{ROOT}/data/hardi/denoised_hardi_SNR-20.nii.gz", data, data_affine)
+    response_mask = select_voxel(gtab, data, roi_radii=10, voxel_nb=200)
+    response, ratio = response_from_mask_ssst(gtab, data, response_mask)
 
-    response, ratio = auto_response_ssst(
-        gtab,
-        data,
-        roi_radii=4,
-        fa_thr=0.3,
-    )
+    lmax = 12
 
-    csd_model = ConstrainedSphericalDeconvModel(
-        gtab,
-        response,
-        sh_order=8,
-    )
-    csd_fit = csd_model.fit(data, mask=mask)
+    #csd_model = MRtrixCSDModel(gtab, response, sh_order=lmax, tau=0.1, convergence=30, eta=1.0)
+    #csd_fit = csd_model.fit(data) # can load mask
+    #csd_coeff = csd_fit.shm_coeff
+    #tournier_base_csd = convert_sh_descoteaux_tournier(csd_coeff)
+    #fods_img_csd = nib.Nifti1Image(tournier_base_csd, data_affine)
+    #nib.save(fods_img_csd,  f"/home/ekin/Desktop/comparison/new/csd{lmax}_v2.nii.gz")
+
 
     sr2csd = SR2CSDModel(
         gtab,
         response,
-        sh_order=12,
-        rho=0.1,
+        sh_order=lmax,
+        rho=1.0,
         tau=0.1,
         convergence=30,
         eta=1.0,
+        denoise_mask=f"{ROOT}/data/hardi/mask.nii",
+        csd_use_mask=False,  # set True only if your CSD prior used a mask
     )
-    sr2csd_fit = sr2csd.fit(data, mask=mask)
 
-    csd_coeff = csd_fit.shm_coeff
+    sr2csd_fit = sr2csd.fit(data)  # uses prior pipeline by default
+
+    #csd_coeff = csd_fit.shm_coeff
     sr2csd_coeff = sr2csd_fit.shm_coeff
 
-    tournier_base_csd = convert_sh_descoteaux_tournier(csd_coeff)
-    fods_img_csd = nib.Nifti1Image(tournier_base_csd, data_affine)
-    nib.save(fods_img_csd,  f"/home/ekin/Desktop/sr2-csd/tests/csd8_test1.nii.gz")
+    #tournier_base_csd = convert_sh_descoteaux_tournier(csd_coeff)
+    #fods_img_csd = nib.Nifti1Image(tournier_base_csd, data_affine)
+    #nib.save(fods_img_csd,  f"/home/ekin/Desktop/sr2-csd/tests/csd8_test1.nii.gz")
 
     tournier_base_sr2csd = convert_sh_descoteaux_tournier(sr2csd_coeff)
     fods_img_sr2csd = nib.Nifti1Image(tournier_base_sr2csd, data_affine)
-    nib.save(fods_img_sr2csd,  f"/home/ekin/Desktop/sr2-csd/tests/sr2csd_test1.nii.gz")
+    nib.save(fods_img_sr2csd,  f"/home/ekin/Desktop/comparison/new/sr2csd_v5.nii.gz")
 
     print("CSD and SR2-CSD ran successfully.")
     print(f"Response ratio (lambda2/lambda1): {ratio:.6f}")
-    print(f"CSD coeff shape: {csd_fit.shm_coeff.shape}")
+    #print(f"CSD coeff shape: {csd_fit.shm_coeff.shape}")
     print(f"SR2 coeff shape: {sr2csd_fit.shm_coeff.shape}")
 
 
